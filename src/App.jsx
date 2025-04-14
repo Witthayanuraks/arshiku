c47b53c0d57046e965034fe5a0e9c90b0734f5b4
Aris Noer Cahya <rienarem@gmail.com>
2025-04-14 10:15:02 +0700

    fix-reupload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/sidebar';
+import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import './index.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
